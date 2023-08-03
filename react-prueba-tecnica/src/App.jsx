696d66bcae9b9c0fb0d92b37a8c643616ade2fd8
augustoriccardi <130772213+augustoriccardi@users.noreply.github.com>
2023-08-03 12:19:15 -0300

    aprendiendo custom hooks

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,54 +1,30 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-
-const CAT_ENDPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
-const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+import { useCatImage } from "./hooks/useCatImage";
+import { useCatFact } from "./hooks/useCatFact";
+import { Otro } from "./Components/Otro";
 
 export function App() {
-  const [fact, setFact] = useState("Lorem impsum cat fact whatever");
-  const [imageUrl, setImageUrl] = useState(null);
+  const { fact, refreshFact } = useCatFact();
+  const { imageUrl } = useCatImage({ fact });
 
-  const getRandomFact = () => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res) => res.json())
-      .then((data) => {
-        const { fact } = data;
-        setFact(fact);
-      });
+  const handleClick = async () => {
+    refreshFact();
   };
-
-  // para recuperar la cita al cargar la página
-  useEffect(getRandomFact, []);
-
-  //para recuperar la imagen cada vez que tenemos una cita nueva
-  useEffect(() => {
-    if (!fact) return;
-    const threeFirstWords = fact.split(" ", 3).join(); // Si son más palabras se puede con split y segundo parametro, o slice y join
-    console.log(threeFirstWords);
-    fetch(
-      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
-    )
-      .then((res) => res.json())
-      .then((response) => {
-        const { url } = response;
-        setImageUrl(url);
-      });
-  }, [fact]);
 
   return (
     <main>
       <h1>App de gatitos</h1>
-      <button onClick={getRandomFact}>Get new fact</button>
+      <button onClick={handleClick}>Get new fact</button>
       <section>
         {fact && <p>{fact}</p>}
         {imageUrl && (
           <img
-            src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+            src={imageUrl}
             alt={`image of a cat extracted using the first three words of the fact ${fact}`}
           />
         )}
       </section>
+      <Otro />
     </main>
   );
 }
